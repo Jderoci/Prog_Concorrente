0da5fd38bef934901bfa8b97b1250e0f80428bff
Jderoci <79822930+Jderoci@users.noreply.github.com>
2023-05-12 02:03:18 -0300

    Update Laboratório_5.c

--- Laboratório_5.c
+++ Laboratório_5.c
@@ -7,8 +7,8 @@
 //Criando as Variáveis Globais
 int x = 0;
 pthread_mutex_t x_mutex;
-pthread_mutex_t x_cond1;
-pthread_mutex_t x_cond2;
+pthread_cond_t x_cond1;
+pthread_cond_t x_cond2;
 
 //Definindo as Threads
 //Thread 1
@@ -16,16 +16,67 @@ void *thread1 (void *t){
     printf("Oi Maria!\n");
     pthread_mutex_lock(&x_mutex);
     x++;
-    
-    if (x==2){
-        
+
+    if(x == 2){
+        pthread_cond_signal(&x_cond1);
     }
+    pthread_mutex_unlock(&x_mutex);
+    pthread_exit(NULL);
 }
 
 //Thread 2
+void *thread2 (void *t){
+    printf("Oi José!\n");
+    pthread_mutex_lock(&x_mutex);
+    x++;
+
+    if(x == 2){
+        pthread_cond_signal(&x_cond1);
+    }
+    pthread_mutex_unlock(&x_mutex);
+    pthread_exit(NULL);
+}
+
 //Thread 3
+void *thread3 (void *t){
+    pthread_mutex_lock(&x_mutex);
+
+    if(x < 2){
+        pthread_cond_wait(&x_cond1, &x_mutex);
+    }
+    x++;
+
+    printf("Sente-se por favor.\n");
+    pthread_cond_signal(&x_cond2);
+    pthread_mutex_unlock(&x_mutex);
+    pthread_exit(NULL);
+}
+
 //Thread 4
+void *thread4 (void *t){
+    pthread_mutex_lock(&x_mutex);
+
+    if(x < 3){
+        pthread_cond_wait(&x_cond2, &x_mutex);
+    }
+  
+    printf("Até mais José!\n");
+    pthread_mutex_unlock(&x_mutex);
+    pthread_exit(NULL);
+}
+
 //Thread 5
+void *thread5 (void *t){
+    pthread_mutex_lock(&x_mutex);
+
+    if(x < 3){
+        pthread_cond_wait(&x_cond2, &x_mutex);
+    }
+  
+    printf("Até mais Maria!\n");
+    pthread_mutex_unlock(&x_mutex);
+    pthread_exit(NULL);
+}
 
 //Função Principal
 int main(int argc, char *argv[]){
@@ -33,9 +84,9 @@ int main(int argc, char *argv[]){
     pthread_t threads[NTHREADS];
 
     //Inicializando os recursos de sincronização
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond1, NULL);
-    pthread_cond_init(&cond2, NULL);
+    pthread_mutex_init(&x_mutex, NULL);
+    pthread_cond_init(&x_cond1, NULL);
+    pthread_cond_init(&x_cond2, NULL);
 
     //Criando as Threads
     pthread_create(&threads[0], NULL, thread1, NULL);
@@ -44,14 +95,14 @@ int main(int argc, char *argv[]){
     pthread_create(&threads[3], NULL, thread4, NULL);
     pthread_create(&threads[4], NULL, thread5, NULL);
 
-    //Esperando as threads completarem
+    //Esperando as Threads completarem
     for (int i = 0; i < NTHREADS; i++){
         pthread_join(threads[i], NULL);
     }
-    printf("\nFIM\n");
+    printf("\nFIM!\n");
     
-    //Liberando os recursos de sincronização
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond1);
-    pthread_cond_destroy(&cond2);
+    //Desalocando as Variáveis e Finalizando
+    pthread_mutex_destroy(&x_mutex);
+    pthread_cond_destroy(&x_cond1);
+    pthread_cond_destroy(&x_cond2);
 }
